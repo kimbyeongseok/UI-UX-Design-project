51704289416091610e509e50e536c0114aa0a6a2
kimbyeongseok <69345685+kimbyeongseok@users.noreply.github.com>
2020-10-08 14:26:25 +0900

    Final version

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,10 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // side nav bar click function
+  $(".side_nav_bars").click(function () {
+    $(this).toggleClass("open");
+    $(".slide_container").toggleClass("active");
+    $(".slide_container .menu_sec a").toggleClass("active");
+    $(".slide_container .menu_sec .sub_menu_sec").toggleClass("active");
+    $(".slide_container .menu_sec p").toggleClass("active");
+  });
+});
